0f07a6a04471260f1e4ab76e364f7482fd0d00ca
carolineferguson <carolineferguson1@outlook.com>
2020-10-27 21:02:18 -0300

    model musica

--- models/musica.ts
+++ models/musica.ts
@@ -3,9 +3,6 @@ import SpotifyWebApi = require("spotify-web-api-node");
 import app = require("../app");
 import Usuario = require("./usuario");
 /*
-const clientId = process.env.clientId;
-const clientSecret = process.env.clientSecret;
-const redirectUri = "http://localhost:1337/callback";
 
 const tracks =  app.ap().getMyTopTracks({
     limit: 10,
@@ -18,13 +15,12 @@ const toptracks :Array <String> = [];
 		toptracks.push(tracks.body.items[i].id);
 		}
 */
-/*export = class MusicaMaisTocadas {
-    public usuario : Usuario;
+/*export = class Musicas {
     public musicas: Array<String> = toptracks;
 
     public static async inserir(usuario:Usuario,musicas:Array<String>): Promise<void>{
         await Sql.conectar(async (sql)=>{
-            await sql.query("INSERT INTO musica_mais_tocada(idusuario,idmusica) VALUES (?,?)",[usuario.idspotify,musicas]);
+            await sql.query("INSERT INTO musicas(idspotify) VALUES (?,?)",[id]);
         })
     }
 }*/
