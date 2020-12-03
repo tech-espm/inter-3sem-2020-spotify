323a9c1f592c6c01ccda28fe43e510dad36262f7
carolineferguson <carolineferguson1@outlook.com>
2020-12-03 19:44:45 -0300

    Script do banco para testes

---
+++ sql/teste.sql
@@ -0,0 +1,45 @@
+ INSERT INTO usuario (idusuario,idspotify,nome,email,url,token,accessToken,refreshToken,validadeToken,criacao,dataAfinidade) VALUES (2,"Teste","Teste","teste@teste.com.br","https://open.spotify.com/user/","teste","teste","teste",1606884561,now(),now());
+ 
+
+
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('51', '2', '40', '1');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('52', '2', '1', '2');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('53', '2', '2', '3');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('54', '2', '3', '4');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('55', '2', '4', '5');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('56', '2', '5', '6');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('57', '2', '6', '7');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('58', '2', '7', '8');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('59', '2', '8', '9');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('60', '2', '9', '10');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('61', '2', '10', '11');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('62', '2', '11', '12');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('63', '2', '12', '13');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('64', '2', '13', '14');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('65', '2', '14', '15');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('66', '2', '15', '16');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('67', '2', '16', '17');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('68', '2', '17', '18');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('69', '2', '18', '19');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('70', '2', '19', '20');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('71', '2', '20', '21');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('72', '2', '21', '22');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('73', '2', '22', '23');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('74', '2', '23', '24');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('75', '2', '24', '25');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('76', '2', '25', '26');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('77', '2', '26', '27');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('78', '2', '27', '28');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('79', '2', '28', '29');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('80', '2', '29', '30');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('81', '2', '30', '31');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('82', '2', '31', '32');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('83', '2', '32', '33');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('84', '2', '33', '34');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('85', '2', '34', '35');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('86', '2', '35', '36');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('87', '2', '36', '37');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('88', '2', '37', '38');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('89', '2', '38', '39');
+INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('90', '2', '39', '40');
+
