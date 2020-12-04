6f3c156004636ad78b760776865d2fc2752cab64
carolineferguson <carolineferguson1@outlook.com>
2020-12-03 21:46:15 -0300

    Script do banco para testes

--- sql/teste.sql
+++ sql/teste.sql
@@ -1,4 +1,4 @@
- INSERT INTO usuario (idusuario,idspotify,nome,email,url,token,accessToken,refreshToken,validadeToken,criacao,dataAfinidade) VALUES (2,"Teste","Teste","teste@teste.com.br","https://open.spotify.com/user/","teste","teste","teste",1606884561,now(),now());
+ INSERT INTO usuario (idusuario,idspotify,nome,email,url,token,accessToken,refreshToken,validadeToken,criacao) VALUES (2,"Teste","Teste","teste@teste.com.br","https://open.spotify.com/user/","teste","teste","teste",1606884561,now());
  
 
 
@@ -43,3 +43,43 @@ INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`,
 INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('89', '2', '38', '39');
 INSERT INTO `spotify`.`musica_mais_tocada` (`idmusica_mais_tocada`, `idusuario`, `idmusica`, `ordem`) VALUES ('90', '2', '39', '40');
 
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (50,2,40,0);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (51,2,41,1);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (52,2,42,2);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (53,2,43,3);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (54,2,1,4);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (55,2,2,5);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (56,2,3,6);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (57,2,4,7);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (58,2,5,8);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (59,2,6,9);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (60,2,7,10);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (61,2,8,11);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (62,2,9,12);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (63,2,10,13);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (64,2,11,14);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (65,2,12,15);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (66,2,13,16);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (67,2,14,17);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (68,2,15,18);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (69,2,16,19);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (70,2,17,20);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (71,2,18,21);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (72,2,19,22);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (73,2,20,23);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (74,2,21,24);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (75,2,22,25);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (76,2,23,26);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (77,2,24,27);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (78,2,25,28);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (79,2,26,29);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (80,2,27,30);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (81,2,28,31);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (82,2,29,32);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (83,2,30,33);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (84,2,31,34);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (85,2,32,35);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (86,2,33,36);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (87,2,34,37);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (88,2,35,38);
+INSERT INTO artista_mais_tocado(idartista_mais_tocado,idusuario,idartista,ordem) VALUES (89,2,36,39);
